5d220c2c9030d804f3fd69773f42bee9f5499259
Minwoo Kim <158142406+IT-Gentleman@users.noreply.github.com>
2025-12-24 15:27:24 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main() {
+
+    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    int a, b;
+    cin>>a>>b;
+    cout<<a*b;
+    
+    return 0;
+}
