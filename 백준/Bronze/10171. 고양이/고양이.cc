73d9e8cea9637f6b3ec943822901e6030714709c
Minwoo Kim <158142406+IT-Gentleman@users.noreply.github.com>
2025-12-24 15:44:22 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void){
+    cout << "\\    /\\" << endl;
+    cout << " )  ( \')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|";
+}
