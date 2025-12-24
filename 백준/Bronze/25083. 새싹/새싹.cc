b2f4f556c4f24eedd6d838a095887344ba07c388
Minwoo Kim <158142406+IT-Gentleman@users.noreply.github.com>
2025-12-24 15:28:05 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+//#define SWAP(x,y,t) ((t) = (x), (x) = (y), (y) = (t))
+
+int main(void) {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	cout << "         ,r\'\"7\n";
+	cout << "r`-_   ,\'  ,/\n";
+	cout << " \\. \". L_r\'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+
+	return 0;
+}
+/*
+		 ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+	  |
+	  |
+
+*/
